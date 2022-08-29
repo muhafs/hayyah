f5df88d06830b1d5433cd926c19e57a7caa381ee
muhamadhaspin <muhamadhaspin@gmail.com>
2022-08-29 20:52:46 +0700

    Changes to be committed: 	modified:   hayyah/app/Http/Controllers/Admin/GalleryController.php 	modified:   hayyah/composer.json 	modified:   hayyah/composer.lock 	modified:   hayyah/config/app.php 	new file:   hayyah/config/image.php

--- hayyah/app/Http/Controllers/Admin/GalleryController.php
+++ hayyah/app/Http/Controllers/Admin/GalleryController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\TravelPackage;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\Admin\GalleryRequest;
 
@@ -45,8 +47,31 @@ class GalleryController extends Controller
     public function store(GalleryRequest $request)
     {
         $data = $request->all();
-        $data['image'] = $request->file('image')->store('assets/gallery', 'public');
+        // $data['image'] = $request->file('image')->store('assets/gallery', 'public');
 
+        // Extract Image File
+        $image = $request->file('image');
+        // Create Image Name
+        $image_name = 'IMG_' . $request->travel_package_id . '_' . time() . '.' . $image->extension();
+
+        // Create Thumbnail's Path
+        $directory = 'assets/gallery';
+        $destination_path = public_path('storage/' . $directory);
+
+        // Check/Create Thumbnail's Directory
+        if (!File::exists($destination_path)) {
+            File::makeDirectory($destination_path, 0755, true);
+        }
+
+        // Store Thumbnail into its destination Path
+        Image::make($image)
+            ->resize(300, 300, function ($constraint) {
+                $constraint->aspectRatio();
+            })
+            ->save($destination_path . '/' . $image_name);
+
+        // Get Full Path and store it into Database
+        $data['image'] = $directory . '/' . $image_name;
         Gallery::create($data);
 
         return redirect()->route('gallery.index')->with('success', 'Gallery Has been created successfully !');
@@ -88,18 +113,36 @@ class GalleryController extends Controller
         $data = $request->only('travel_package_id');
 
         if ($request->hasFile('image')) {
+            // If there is an Old Image, Delete it first
             if ($gallery->image != null && Storage::disk('public')->exists($gallery->image)) {
                 Storage::disk('public')->delete($gallery->image);
             }
 
-            $data['image'] = $request->file('image')->store('assets/gallery', 'public');
+            // $data['image'] = $request->file('image')->store('assets/gallery', 'public');
 
-            // $data['image'] = $request->file('image')
-            //     ->storeAs(
-            //         'assets/gallery', // Path
-            //         'Test_' . $request->travel_package_id . '_' . $request->file('image')->extension(), // Name
-            //         'public' // Option
-            //     );
+            // Extract Image File
+            $image = $request->file('image');
+            // Create Image Name
+            $image_name = 'IMG_' . $request->travel_package_id . '_' . time() . '.' . $image->extension();
+
+            // Create Thumbnail's Path
+            $directory = 'assets/gallery';
+            $destination_path = public_path('storage/' . $directory);
+
+            // Check/Create Thumbnail's Directory
+            if (!File::exists($destination_path)) {
+                File::makeDirectory($destination_path, 0755, true);
+            }
+
+            // Store Thumbnail into its destination Path
+            Image::make($image)
+                ->resize(300, 300, function ($constraint) {
+                    $constraint->aspectRatio();
+                })
+                ->save($destination_path . '/' . $image_name);
+
+            // Get Full Path and store it into Database
+            $data['image'] = $directory . '/' . $image_name;
         }
 
         $gallery->update($data);
